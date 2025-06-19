d4c95273ab390e0e4a4a7cbf7020c77eb35f5209
Nati Fessh <natitesfaldet111@gmail.com>
2025-06-19 18:32:17 +0300

    fixed bugs migrated db to pg

--- src/db.rs
+++ src/db.rs
@@ -1,28 +1,47 @@
-use sqlx::sqlite::SqlitePool;
+use std::env;
+
+use base64::{engine, Engine};
+use dotenv::dotenv;
+use rand::Rng;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::*;
 use uuid::Uuid;
+
+use crate::OrginalUrl;
 #[derive(Debug,Clone)]
 pub struct Db{
-    con:SqlitePool
+    con:Pool<Postgres>
 }
 impl Db {
    pub  async fn new()->Result<Self,sqlx::Error>{
-        let conn=sqlite::SqlitePool::connect("sqlite://url.db").await?;
-        sqlx::query("CREATE TABLE IF NOT EXISTS URLS(ID INTEGER PRIMARY KEY,SHORTENED TEXT NOT NULL UNIQUE,ORGINAL TEXT NOT NULL)",).execute(&conn).await?;
-
-        Ok(Db { con: conn })
-        
-    }
+        dotenv().ok();
+        let db_url=env::var("PG_URL").expect("DATABASE_URL must be set");
+        let conn=PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await.expect("failed");
+        sqlx::query(
+        "CREATE TABLE IF NOT EXISTS urls (
+            id SERIAL PRIMARY KEY,
+            shortened TEXT NOT NULL UNIQUE,
+            original TEXT NOT NULL
+        )"
+    )
+    .execute(&conn)
+    .await?;
+        Ok(Db {con:conn})
+   }   
    pub  async fn insert_url(&self,  orginal:String)->Result<String,sqlx::Error> {
-    let shortened=Uuid::new_v4().to_string();
-    sqlx::query("INSERT INTO URLS(SHORTENED,ORGINAL) VALUES(?1,?2)").bind(&shortened).bind(orginal).execute(&self.con).await?;
+    let mut  rand=rand::thread_rng();
+    let rand_bytes:[u8;6]=rand.gen();
+    let shortened=engine::general_purpose::URL_SAFE.encode(rand_bytes);
+    sqlx::query("INSERT INTO URLS(shortened,original) VALUES($1,$2)").bind(&shortened).bind(orginal).execute(&self.con).await.expect("qhattt");
     Ok(shortened)
         
     }
     pub async fn get_orginal(&self,shortened:String)->Result<String,sqlx::Error>{
-        let row=sqlx::query("SELECT ORGINAL FROM URLS WHERE SHORTENED=?1").bind(shortened).fetch_one(&self.con).await?;
-        let orginal:String=row.get(0);
-        Ok(orginal)
+        let row:OrginalUrl=sqlx::query_as::<_,OrginalUrl>("SELECT original FROM URLS WHERE SHORTENED=$1").bind(shortened).fetch_one(&self.con).await?;
+        Ok(row.original)
     }
     
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use actix_web::*;
+use std::env;
+
+use actix_cors::Cors;
+use actix_web::{middleware::Logger, *};
 use db::Db;
 
 use actix_web::HttpResponse;
 
 use dev::WebService;
+use env_logger::Env;
 use serde::{Deserialize, Serialize};
-#[derive(Deserialize)]
+use sqlx::prelude::FromRow;
+#[derive(Deserialize,FromRow,Debug,Default)]
 pub struct OrginalUrl{
-    original:String
+   // #[serde(rename = "ORIGINAL")]
+    original:String,
+  
 }
 #[derive(Serialize)]
 struct ShortenedUrl{
@@ -17,43 +24,48 @@ struct ShortenedUrl{
 #[post("/shorten")]
 pub async fn shorten_url(db:web::Data<Db>,req:web::Json<OrginalUrl>)->impl Responder{
     match db.insert_url(req.original.clone()).await {
-        Ok(shortened)=>HttpResponse::Ok().json(ShortenedUrl{shortened}),
-        Err(_)=>HttpResponse::InternalServerError().body("Error saving url")
-        
+        Ok(shortened)=>HttpResponse::Ok().json(ShortenedUrl{shortened:shortened}),
+        Err(_)=>HttpResponse::InternalServerError().body("Error saving url")  
     }
 }
 #[get("/{shortened}")]
 pub async fn get_full_url(db:web::Data<Db>,req:HttpRequest)->impl Responder{
     let shortened=req.match_info().get("shortened").unwrap_or_default();
-   
     match db.get_orginal(String::from(shortened)).await {
         Ok(orginal)=>HttpResponse::Found().insert_header(("location",orginal)).finish(),
-        Err(_)=>HttpResponse::NotFound().body("url not found")
-
-        
+        Err(_)=>HttpResponse::NotFound().body("url not found")   
     }
-    
 }
 pub fn config(conf:&mut web::ServiceConfig){
     conf.service(get_full_url);
     conf.service(shorten_url);
-
-
 }
+
+
 
 mod db;
 #[actix_web::main]
 async fn main() ->std::io::Result<()>{
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("8080"));
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init_from_env(Env::default().default_filter_or("mehhh"));
     let db=Db::new().await.unwrap();
     HttpServer::new(move ||{
-        
+        let cors = Cors::default()
+        .allow_any_origin()
+        .allowed_methods(vec!["GET", "POST"])
+        .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+        .allowed_header(http::header::CONTENT_TYPE)
+        .max_age(3600);
         App::new()
+        .wrap(cors)
+        .wrap(Logger::default()) 
         .app_data(web::Data::new(db.clone()))
        
             .configure(config)
-    }).bind("127.0.0.1:8080")?
+    }).bind(format!("0.0.0.0:{}",port))?
     .run()
     .await
-   
 }
+
 
